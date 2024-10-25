69b556f4a84309022272fece23dd59e34fe89ea5
DomoChip <domochip@hotmail.com>
2024-10-25 22:19:36 +0200

    Update WirelessBase to 2.4.0

--- src/base/Core.cpp
+++ src/base/Core.cpp
@@ -40,7 +40,20 @@ String Core::generateStatusJSON()
 
   return gs;
 }
-bool Core::appInit(bool reInit = false) { return true; };
+bool Core::appInit(bool reInit = false)
+{
+
+// init ticker to check for update every 24h
+#ifdef ESP8266
+  _checkForUpdateTicker.attach(86400, [this]()
+                               { this->_needCheckForUpdateTick = true; });
+#else
+  _checkForUpdateTicker.attach<typeof this>(86400, [](typeof this core)
+                                            { core->_needCheckForUpdateTick = true; }, this);
+#endif
+
+  return true;
+};
 const PROGMEM char *Core::getHTMLContent(WebPageForPlaceHolder wp)
 {
   switch (wp)
@@ -104,103 +117,43 @@ void Core::appInitWebServer(WebServer &server, bool &shouldReboot, bool &pauseAp
     server.sendHeader(F("Content-Encoding"), F("gzip"));
     server.send_P(200, PSTR("text/html"), fwhtmlgz, sizeof(fwhtmlgz)); });
 
-  server.on("/discover.html", HTTP_GET, [this, &server]()
+  // Get Update Infos ---------------------------------------------------------
+  server.on("/gui", HTTP_GET, [this, &server]()
             {
     SERVER_KEEPALIVE_FALSE()
-    server.sendHeader(F("Content-Encoding"), F("gzip"));
-    server.send_P(200, PSTR("text/html"), discoverhtmlgz, sizeof(discoverhtmlgz)); });
+    server.send(200, F("application/json"), getUpdateInfos(server.hasArg("refresh"))); });
 
-  // sn url is a way to find module on network --------------------------------
-  char discoURL[10];
-#ifdef ESP8266
-  sprintf_P(discoURL, PSTR("/%08x"), ESP.getChipId());
-#else
-  sprintf_P(discoURL, PSTR("/%08x"), (uint32_t)(ESP.getEfuseMac() << 40 >> 40));
-#endif
-  server.on(discoURL, HTTP_GET, [&server]()
+  // Update Firmware from Github ----------------------------------------------
+  server.on("/update", HTTP_POST, [this, &shouldReboot, &pauseApplication, &server]()
             {
-    char chipID[9];
-#ifdef ESP8266
-    sprintf_P(chipID, PSTR("%08x"), ESP.getChipId());
-#else
-    sprintf_P(chipID, PSTR("%08x"), (uint32_t)(ESP.getEfuseMac() << 40 >> 40));
-#endif
-    SERVER_KEEPALIVE_FALSE()
-    server.enableCORS(true); //allow this URL to be requested from everywhere
-    server.sendHeader(F("Cache-Control"), F("no-cache"));
-    server.send(200, "text/html", chipID); });
+              shouldReboot = updateFirmware(server.arg("plain").c_str());
 
-  // ffffffff url is a way to find all modules on the network -----------------
-  server.on("/ffffffff", HTTP_GET, [&server]()
-            {
-    //answer with a JSON string containing sn, model and version numbers
-    char discoJSON[128];
-#ifdef ESP8266
-    sprintf_P(discoJSON, PSTR("{\"sn\":\"%08x\",\"m\":\"%s\",\"v\":\"%s\"}"), ESP.getChipId(), APPLICATION1_NAME, VERSION);
-#else
-    sprintf_P(discoJSON, PSTR("{\"sn\":\"%08x\",\"m\":\"%s\",\"v\":\"%s\"}"), (uint32_t)(ESP.getEfuseMac() << 40 >> 40), APPLICATION1_NAME, VERSION);
-#endif
-    SERVER_KEEPALIVE_FALSE()
-    server.enableCORS(true); //allow this URL to be requested from everywhere
-    server.sendHeader(F("Cache-Control"), F("no-cache"));
-    server.send(200, "text/json", discoJSON); });
+              SERVER_KEEPALIVE_FALSE()
+              if (shouldReboot)
+                server.send(200, F("text/html"), F("Firmware Successfully Updated"));
+              else
+                server.send(500, F("text/html"), F("Firmware Update Failed")); });
 
-  // FirmWare POST URL allows to push new firmware ----------------------------
+  // Firmware POST URL allows to push new firmware ----------------------------
   server.on(
       "/fw", HTTP_POST, [&shouldReboot, &pauseApplication, &server]()
       {
     shouldReboot = !Update.hasError();
-    if (shouldReboot) {
+    if (shouldReboot)
+    {
       SERVER_KEEPALIVE_FALSE()
-      server.send(200, F("text/html"), F("Firmware Successfully Uploaded<script>setTimeout(function(){if('referrer' in document)window.location=document.referrer;},10000);</script>"));
+      server.send(200, F("text/html"), F("Firmware Successfully Updated"));
     }
-    else {
-      //Upload failed so restart to Run Application in loop
+    else
+    {
+      // Upload failed so restart to Run Application in loop
       pauseApplication = false;
-      //Prepare response
-      String errorMsg(Update.getError());
-      errorMsg+=' ';
+      // Prepare response
+      String errorMsg;
 #ifdef ESP8266
-      switch(Update.getError()){
-        case UPDATE_ERROR_WRITE:
-          errorMsg=F("Flash Write Failed");
-          break;
-        case UPDATE_ERROR_ERASE:
-          errorMsg=F("Flash Erase Failed");
-          break;
-        case UPDATE_ERROR_READ:
-          errorMsg=F("Flash Read Failed");
-          break;
-        case UPDATE_ERROR_SPACE:
-          errorMsg=F("Not Enough Space");
-          break;
-        case UPDATE_ERROR_SIZE:
-          errorMsg=F("Bad Size Given");
-          break;
-        case UPDATE_ERROR_STREAM:
-          errorMsg=F("Stream Read Timeout");
-          break;
-        case UPDATE_ERROR_MD5:
-          errorMsg=F("MD5 Check Failed");
-          break;
-        case UPDATE_ERROR_FLASH_CONFIG:
-          errorMsg=F("Flash config wrong");
-          break;
-        case UPDATE_ERROR_NEW_FLASH_CONFIG:
-          errorMsg=F("New Flash config wrong");
-          break;
-        case UPDATE_ERROR_MAGIC_BYTE:
-          errorMsg=F("Magic byte is wrong, not 0xE9");
-          break;
-        case UPDATE_ERROR_BOOTSTRAP:
-          errorMsg=F("Invalid bootstrapping state, reset ESP8266 before updating");
-          break;
-        default:
-          errorMsg=F("Unknown error");
-          break;
-      }
+      errorMsg = Update.getErrorString();
 #else
-      errorMsg=Update.errorString();
+      errorMsg = Update.errorString();
 #endif
       SERVER_KEEPALIVE_FALSE()
       server.send(500, F("text/html"), errorMsg);
@@ -208,49 +161,38 @@ void Core::appInitWebServer(WebServer &server, bool &shouldReboot, bool &pauseAp
       [&pauseApplication, &server]()
       {
         HTTPUpload &upload = server.upload();
+
         if (upload.status == UPLOAD_FILE_START)
         {
           // stop to Run Application in loop
           pauseApplication = true;
 
 #ifdef LOG_SERIAL
+          // Set Update onError callback
+          Update.onError([](uint8_t err)
+                         { Update.printError(LOG_SERIAL); });
+
           LOG_SERIAL.printf("Update Start: %s\n", upload.filename.c_str());
 #endif
 
 #ifdef ESP8266
-          if (!Update.begin((ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000))
-          {
+          Update.begin((ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000);
 #else
-          if (!Update.begin())
-          {
+          Update.begin();
 #endif
-#ifdef LOG_SERIAL
-            Update.printError(LOG_SERIAL);
-#endif
-          }
         }
         else if (upload.status == UPLOAD_FILE_WRITE)
         {
-          if (Update.write(upload.buf, upload.currentSize) != upload.currentSize)
-          {
-#ifdef LOG_SERIAL
-            Update.printError(LOG_SERIAL);
-#endif
-          }
+          Update.write(upload.buf, upload.currentSize);
         }
         else if (upload.status == UPLOAD_FILE_END)
         {
           if (Update.end(true))
-          {
 #ifdef LOG_SERIAL
             LOG_SERIAL.printf("Update Success: %uB\n", upload.totalSize);
-          }
-          else
-          {
-            Update.printError(LOG_SERIAL);
 #endif
-          }
         }
+
 #ifdef ESP8266
         // Feed the dog otherwise big firmware won't pass
         ESP.wdtFeed();
@@ -262,14 +204,14 @@ void Core::appInitWebServer(WebServer &server, bool &shouldReboot, bool &pauseAp
   server.on("/rbt", HTTP_POST, [&shouldReboot, &server]()
             {
     SERVER_KEEPALIVE_FALSE()
-    server.send_P(200,PSTR("text/html"),PSTR("Reboot command received<script>setTimeout(function(){if('referrer' in document)window.location=document.referrer;},30000);</script>"));
+    server.send_P(200,PSTR("text/html"),PSTR("Reboot command received"));
     shouldReboot = true; });
 
   // reboot RescueMode POST ---------------------------------------------------
   server.on("/rbtrsc", HTTP_POST, [&shouldReboot, &server]()
             {
     SERVER_KEEPALIVE_FALSE()
-    server.send_P(200,PSTR("text/html"),PSTR("Reboot in rescue command received<script>setTimeout(function(){if('referrer' in document)window.location=document.referrer;},30000);</script>"));
+    server.send_P(200,PSTR("text/html"),PSTR("Reboot in rescue command received"));
     //Set EEPROM for Rescue mode flag
     EEPROM.begin(4);
     EEPROM.write(0, 1);
@@ -281,4 +223,231 @@ void Core::appInitWebServer(WebServer &server, bool &shouldReboot, bool &pauseAp
                     {
     SERVER_KEEPALIVE_FALSE()
     server.send(404); });
+}
+
+void Core::appRun()
+{
+  if (_needCheckForUpdateTick)
+  {
+    _needCheckForUpdateTick = false;
+    // check for update
+    checkForUpdate();
+  }
+}
+
+Core::Core(char appId, String appName) : Application(appId, appName)
+{
+  _applicationList[Application::Applications::Core] = this;
+}
+
+void Core::checkForUpdate()
+{
+  String githubURL = "https://api.github.com/repos/" APPLICATION1_MANUFACTURER "/" APPLICATION1_MODEL "/releases/latest";
+
+  WiFiClientSecure clientSecure;
+  HTTPClient http;
+
+  clientSecure.setInsecure();
+  http.begin(clientSecure, githubURL);
+  int httpCode = http.GET();
+  if (httpCode == 200)
+  {
+    WiFiClient *stream = http.getStreamPtr();
+    JsonDocument doc;
+    DeserializationError error = deserializeJson(doc, *stream);
+    if (!error)
+    {
+      JsonVariant jv;
+      if ((jv = doc["tag_name"]).is<const char *>())
+        strlcpy(_lastFirmwareInfos.version, jv, sizeof(_lastFirmwareInfos.version));
+      if ((jv = doc["name"]).is<const char *>())
+      {
+        // find the first space and copy the rest to title
+        char *space = strchr(jv, ' ');
+        if (space)
+          strlcpy(_lastFirmwareInfos.title, space + 1, sizeof(_lastFirmwareInfos.title));
+        else
+          _lastFirmwareInfos.title[0] = 0;
+      }
+
+      if ((jv = doc["published_at"]).is<const char *>())
+        strlcpy(_lastFirmwareInfos.releaseDate, jv, sizeof(_lastFirmwareInfos.releaseDate));
+      else
+        _lastFirmwareInfos.releaseDate[0] = 0;
+
+      if ((jv = doc["body"]).is<const char *>())
+      {
+        // copy body to summary until "\r\n\r\n##"
+        const char *body = jv.as<const char *>();
+        const char *end = strstr(body, "\r\n\r\n##");
+        if (end)
+        {
+          size_t len = end - body;
+          if (len >= sizeof(_lastFirmwareInfos.summary))
+            len = sizeof(_lastFirmwareInfos.summary) - 1;
+
+          // Copy the string considering multi-byte characters
+          strncpy(_lastFirmwareInfos.summary, body, len);
+          _lastFirmwareInfos.summary[len] = 0;
+        }
+        else
+        {
+          size_t bodyLen = strlen(body);
+          if (bodyLen < sizeof(_lastFirmwareInfos.summary))
+          {
+            strncpy(_lastFirmwareInfos.summary, body, bodyLen);
+            _lastFirmwareInfos.summary[bodyLen] = 0;
+          }
+          else
+            _lastFirmwareInfos.summary[0] = 0;
+        }
+      }
+    }
+    else
+      _lastFirmwareInfos.version[0] = 0;
+  }
+  else
+    _lastFirmwareInfos.version[0] = 0;
+
+  http.end();
+}
+
+String Core::getUpdateInfos(bool refresh)
+{
+  if (refresh)
+    checkForUpdate();
+
+  JsonDocument doc;
+
+  doc["installed_version"] = VERSION;
+  if (_lastFirmwareInfos.version[0])
+  {
+    doc["latest_version"] = _lastFirmwareInfos.version;
+    doc["title"] = _lastFirmwareInfos.title;
+    doc["release_date"] = _lastFirmwareInfos.releaseDate;
+    doc["release_summary"] = _lastFirmwareInfos.summary;
+    doc["release_url"] = String(F("https://github.com/" APPLICATION1_MANUFACTURER "/" APPLICATION1_MODEL "/releases/tag/")) + _lastFirmwareInfos.version;
+  }
+
+  String infos;
+  serializeJson(doc, infos);
+
+  return infos;
+}
+
+bool Core::updateFirmware(const char *version)
+{
+  char versionToFlash[8];
+
+  if (version && !strcmp(version, "latest"))
+  {
+    checkForUpdate();
+    if (_lastFirmwareInfos.version[0] and versionCompare(_lastFirmwareInfos.version, VERSION) > 0)
+      strlcpy(versionToFlash, _lastFirmwareInfos.version, sizeof(versionToFlash));
+    else
+      return false;
+  }
+  else if (version && version[0])
+    strlcpy(versionToFlash, version, sizeof(versionToFlash));
+  else
+    return false;
+
+  WiFiClientSecure clientSecure;
+  clientSecure.setInsecure();
+
+  String fwUrl(F("https://github.com/" APPLICATION1_MANUFACTURER "/" APPLICATION1_MODEL "/releases/download/"));
+  fwUrl = fwUrl + versionToFlash + '/' + APPLICATION1_MODEL + '.' + versionToFlash + F(".bin");
+
+#ifdef LOG_SERIAL
+  LOG_SERIAL.print(F("Trying to Update from URL: "));
+  LOG_SERIAL.println(fwUrl);
+#endif
+
+  HTTPClient https;
+  https.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
+  https.begin(clientSecure, fwUrl);
+  int httpCode = https.GET();
+
+  if (httpCode != 200)
+  {
+#ifdef LOG_SERIAL
+    LOG_SERIAL.print(F("Failed to download file, httpCode: "));
+    LOG_SERIAL.println(httpCode);
+#endif
+    https.end();
+    return false;
+  }
+
+  // starting here we have a valid httpCode (200)
+
+  // get the stream
+  WiFiClient *stream = https.getStreamPtr();
+  int contentLength = https.getSize();
+
+#ifdef LOG_SERIAL
+  // Set Update onError callback
+  Update.onError([](uint8_t err)
+                 { Update.printError(LOG_SERIAL); });
+
+  LOG_SERIAL.println(F("Firmware file found, Update Start"));
+#endif
+
+#ifdef ESP8266
+  Update.begin(contentLength);
+#else
+  Update.begin();
+#endif
+
+  Update.writeStream(*stream);
+
+  if (Update.end())
+  {
+#ifdef LOG_SERIAL
+    LOG_SERIAL.printf("Update Success: %uB\n", contentLength);
+#endif
+  }
+
+  https.end();
+
+  return !Update.hasError();
+}
+
+int8_t Core::versionCompare(const char *version1, const char *version2)
+{
+  int8_t result = 0;
+
+  // Create copies of the version strings
+  char *v1 = strdup(version1);
+  char *v2 = strdup(version2);
+
+  // Tokenize the version strings
+  char *token1 = strtok(v1, ".");
+  char *token2 = strtok(v2, ".");
+
+  while (token1 && token2)
+  {
+    int v1 = atoi(token1);
+    int v2 = atoi(token2);
+
+    if (v1 > v2)
+    {
+      result = 1;
+      break;
+    }
+    else if (v1 < v2)
+    {
+      result = -1;
+      break;
+    }
+
+    // Move to the next token
+    token1 = strtok(NULL, ".");
+    token2 = strtok(NULL, ".");
+  }
+
+  // Clean up
+  free(v1);
+  free(v2);
+
+  return result;
 }
--- src/base/Core.h
+++ src/base/Core.h
@@ -3,18 +3,32 @@
 
 #include "../Main.h"
 #include "Application.h"
-#ifdef ESP32
+#ifdef ESP8266
+#include <ESP8266HTTPClient.h>
+#else
+#include <HTTPClient.h>
 #include <Update.h>
 #endif
 
 #include "data/status0.html.gz.h"
 #include "data/config0.html.gz.h"
 #include "data/fw.html.gz.h"
-#include "data/discover.html.gz.h"
 
 class Core : public Application
 {
 private:
+  typedef struct
+  {
+    char version[8] = {0};
+    char title[64] = {0};
+    char releaseDate[11] = {0};
+    char summary[255] = {0};
+  } LastFirmwareInfos;
+
+  LastFirmwareInfos _lastFirmwareInfos;
+  Ticker _checkForUpdateTicker;
+  bool _needCheckForUpdateTick = false;
+
   void setConfigDefaultValues();
   bool parseConfigJSON(JsonDocument &doc, bool fromWebPage);
   String generateConfigJSON(bool clearPassword);
@@ -23,10 +37,15 @@ private:
   const PROGMEM char *getHTMLContent(WebPageForPlaceHolder wp);
   size_t getHTMLContentSize(WebPageForPlaceHolder wp);
   void appInitWebServer(WebServer &server, bool &shouldReboot, bool &pauseApplication);
-  void appRun() {};
+  void appRun();
 
 public:
-  Core(char appId, String appName) : Application(appId, appName) { _applicationList[Application::Applications::Core] = this; }
+  Core(char appId, String appName);
+
+  void checkForUpdate();
+  String getUpdateInfos(bool refresh = false);
+  bool updateFirmware(const char *version = nullptr);
+  static int8_t versionCompare(const char *version1, const char *version2);
 };
 
 #endif
